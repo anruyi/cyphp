9988a912d4b3ec989e0c808519cf700f042f9213
CY <123@qq.com>
2017-08-24 13:51:22 +0800

    core

---
+++ core/lib/Log.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenyi
+ * Date: 17-8-23
+ * Time: 9:24
+ */
+
+namespace core\lib;
+
+class Log
+{
+
+    /**
+     * @var
+     * 选择调用的控制方法，是file存储还是mysql存储
+     */
+    static $class;
+
+    static public function init()
+    {
+        $drive = Conf::get('DRIVE','log');
+        $class = '\core\lib\drive\log\\'.$drive;
+        self::$class = new $class; //把弄过来的变量地址作为新的对象，神来之作。
+        p($drive);
+    }
+    static public function log($name,$file='log')
+    {
+        p(self::$class);
+        self::$class->log($name,$file);
+    }
+}
