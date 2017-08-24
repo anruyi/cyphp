97e835d776b647f3eb0955ad0bef7b612b79b18f
CY <123@qq.com>
2017-08-24 12:53:44 +0800

    core

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * 入口文件
- * 1.定义常量CYPHP\CORE\APP
- * 2.加载函数库cyphp
- * 3.启动框架run()
+ * 入口文件，index.php
  * Date: 17-8-18
  * Time: 11:15
  */
@@ -13,13 +10,24 @@ use core\CyPHP;
 define('CYPHP',realpath('./'));
 //核心文件core所处目录
 define('CORE',CYPHP.'/core');
-//项目文件：控制器、模型、、、
+//项目文件：控制器、模型……
 define('APP',CYPHP.'/app');
 //调试
 define('DEBUG',true);
 define('MODEL','/app');
 
+include "vendor/autoload.php";
+
 if(DEBUG){
+    /**
+     * 开源错误提示
+     */
+    $whoops = new \Whoops\Run;
+    $errorTitle = '框架出错!';
+    $option = new \Whoops\Handler\PrettyPageHandler();
+    $option->setPageTitle($errorTitle);
+    $whoops->pushHandler($option);
+    $whoops->register();
     //display_errors 错误提示，init_set方法可以修改php配置
     ini_set('display_errors','On');
 }else{
@@ -29,7 +37,9 @@ if(DEBUG){
 include CORE.'/common/function.php';
 include CORE.'/CyPHP.php';
 
-//不知道什么原因，core\不能删除
+//设置自动加载,传递过去的参数是当前缺乏的文件地址
 spl_autoload_register("core\CyPHP::load");
+
+
 
 CyPHP::run();
